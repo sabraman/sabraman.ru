8e18f1bf76f9fee235d9f5dc5312bda9fae918c3
sabraman <sabraman@yandex.ru>
2026-03-17 04:30:23 +0300

    Remove stale next-intl config plugin

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
  * for Docker builds.
  */
 import "./src/env.js";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -112,4 +109,4 @@ const config = {
 	},
 };
 
-export default withNextIntl(config);
+export default config;
